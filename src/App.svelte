f76ebeda05edd465a2fbde1dafec6718dfd415df
jonatan hilden <jonatan.hilden@gmail.com>
2021-12-14 19:51:06 +0200

    Add contrast calculations
    - add ACPA and WCAG2 contrast calculations

--- src/App.svelte
+++ src/App.svelte
@@ -1,40 +1,65 @@
 <script>
 	import chroma from "chroma-js";
 
-	import { APCAcontrast, sRGBtoY, displayP3toY, colorParsley } from 'apca-w3';
+	import { APCAcontrast, sRGBtoY, displayP3toY, colorParsley } from "apca-w3";
+	import { hex } from "wcag-contrast";
 
-	let name = 'colors grid';
-	let defaultColors = "'#ffffb2', '#fed976', '#feb24c', '#fd8d3c', '#f03b20', '#bd0026'";
+	let name = "colors grid";
+	let defaultColors =
+		"'#ffffb2', '#fed976', '#feb24c', '#fd8d3c', '#f03b20', '#bd0026'";
 
 	let inputcolors = "#ffffb2, #fed976, #feb24c, #fd8d3c, #f03b20, #bd0026";
-	let colors = inputcolors.split(',')
-			//let colors = ['#ffffb2','#fed976','#feb24c','#fd8d3c','#f03b20','#bd0026'];
-		let colorsR = colors.reverse();
+	let colors = inputcolors.split(", ");
+	//let colors = ['#ffffb2','#fed976','#feb24c','#fd8d3c','#f03b20','#bd0026'];
+	let colorsR = colors.reverse();
 
 	let getContrastLc = (textColor, backgroundColor) => {
-		return APCAcontrast( sRGBtoY( colorParsley(textColor) ), sRGBtoY( colorParsley(backgroundColor) ) );}
+		return APCAcontrast(
+			sRGBtoY(colorParsley(textColor)),
+			sRGBtoY(colorParsley(backgroundColor))
+		);
+	};
 
 	let invertTextColor = (textColor, backgroundColor) => {
-		let Lc = getContrastLc(textColor, backgroundColor)
-		console.log(Lc)
-		if (Lc<45){
-			return 'white'
-		} else return 'black'
-	} 
-	
-	let f = ()=>{
-		colors =[]
-		if(inputcolors.length>0){
-			
-		colors = inputcolors.replaceAll("'", "").split(',')
+		let Lc = getContrastLc(textColor, backgroundColor);
+		if (Lc < 45) {
+			return "white";
+		} else return "black";
+	};
+	let f = () => {
+		colors = [];
+		if (inputcolors.length > 0) {
+			colors = inputcolors.replaceAll("'", "").split(", ");
 			colorsR = colors.reverse();
-			console.log(colors)
-	}
-	}
-	let ls = ['a', 'b', 'c']
-	let chromatest = chroma.scale(['white', 'red']).mode('lab');
-	console.log(chromatest)
+			console.log(colors);
+		}
+	};
 
+	let calculateContrast = (colors, colorsR)=>{
+		let contrast = []
+		colors.forEach(clrX => {
+			let row = []
+			colorsR.forEach(clrY =>{
+				row.push({"lC":getContrastLc(clrX, clrY),
+			"cR":hex(clrX, clrY).toFixed(2)})
+			});
+			contrast.push(row)
+		});
+		return contrast
+	}
+
+
+	$: contrast =calculateContrast(colors, colorsR); 
+
+
+	let ls = ["a", "b", "c"];
+	let chromatest = chroma.scale(["white", "red"]).mode("lab");
+	// console.log(chromatest)
+	console.log(
+		hex(colors[0].replace(" ", ""), colors[1].replace(" ", "")),
+		colors[0].replace(" ", "")
+	);
+	let Lc;
 </script>
 
 <input bind:value={inputcolors} placeholder="input colors" on:input={f} />
@@ -43,11 +68,37 @@
 	{#each colors as codeX, j}
 		<div class="row">
 			{#each colorsR as codeY, i}
-				<div class="cell" style="background-color:{codeX}">
-					<div class="inner" style="background-color:{codeY}; color:{invertTextColor('black', codeY)}">
-						{getContrastLc(codeY, codeX)}
+				<div class="cell" style="background-color:{codeX}; 
+			color:{invertTextColor(
+							'black',
+							codeY
+						)}">
+					{#if codeX === codeY}
+					{codeX}
+					{:else}
+					<div
+						class="inner"
+						style="background-color:{codeY}; color:{invertTextColor(
+							'black',
+							codeY
+						)}"
+					>
+						<div class="scoreValue">
+							{#if codeX != codeY}
+							{#if Math.abs(contrast[i][j].lC)>60}
+							<strong>{contrast[i][j].lC}</strong>
+							{:else}
+							{contrast[i][j].lC}
+							{/if}
+							<br />
+								——
+								<br />
+								{contrast[i][j].cR}
+								
+							{/if}
+						</div>
 					</div>
-					{#if codeX === codeY}{codeX}{/if}
+					{/if}
 				</div>
 			{/each}
 		</div>
@@ -80,13 +131,20 @@
 		justify-content: center;
 		flex-wrap: nowrap;
 		flex-direction: column;
+		font-size: 1rem;
 	}
 	.inner {
 		margin: 0.5em;
-		width: 2em;
-		height: 2em;
+		width: 4em;
+		height: 4em;
 		border-radius: 2em;
 		font-size: 0.75em;
-		line-height:2em;
+		line-height: 1em;
+	}
+	.scoreValue {
+		padding-top:0.5em;
+	}
+	strong{
+		font-weight: 900;
 	}
 </style>
